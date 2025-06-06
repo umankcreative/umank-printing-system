15ae70ad2a16b021ed640cc00d9f38dc88f90682
umankcreative <luqmanul.hakeem@gmail.com>
2025-06-06 13:20:40 +0800

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH || '/umank-printing-system', // Set the base path for the application
   optimizeDeps: {
     exclude: ['lucide-react'],
-    base:process.env.VITE_BASE_PATH || '/umank-printing-system', // Set the base path for the application
   },
 });
